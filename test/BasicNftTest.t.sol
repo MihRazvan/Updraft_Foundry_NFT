e526970ef75d8f933b79effd704715459bd079d8
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-10-12 23:13:04 +0300

    create makefile

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,6 +9,11 @@ contract BasicNftTest is Test {
     DeployBasicNft deployer;
     BasicNft basicNft;
 
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    address public USER = makeAddr("user");
+
     function setUp() external {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
@@ -17,6 +22,21 @@ contract BasicNftTest is Test {
     function testNameIsCorrect() public {
         string memory expectedName = "Dogie";
         string memory actualName = basicNft.name();
-        assertEq(expectedName, actualName);
+        //nu e corect, assertEq nu poate compara strings
+        //assertEq(expectedName, actualName);
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMindAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
