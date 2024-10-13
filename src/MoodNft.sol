5177d3050e5efc069b70a9340426bb4fa1dea9da
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-10-13 18:09:20 +0300

    initialize constructor and mint function

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,7 +2,28 @@
 pragma solidity ^0.8;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
-    constructor() ERC721("Mood NFT", "MN") {}
+    uint256 private s_tokenCounter;
+    string private s_happySvgImageUri;
+    string private s_sadSvgImageUri;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_happySvgImageUri = happySvgImageUri;
+        s_sadSvgImageUri = sadSvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
 }
